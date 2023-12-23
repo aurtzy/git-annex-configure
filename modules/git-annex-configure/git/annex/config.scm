a9bed555535f0520f6602a07952d1f11ae59916c
aurtzy <aurtzy@gmail.com>
2023-12-23 00:42:52 -0500

    Change copyright headers to better conform to conventions

--- modules/git-annex-configure/git/annex/config.scm
+++ modules/git-annex-configure/git/annex/config.scm
@@ -1,19 +1,19 @@
-;; Copyright (C) 2023 aurtzy <aurtzy@gmail.com>
-;;
-;; This file is part of git-annex-configure.
-;;
-;; git-annex-configure is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by the Free
-;; Software Foundation, either version 3 of the License, or (at your option) any
-;; later version.
-;;
-;; git-annex-configure is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-;; details.
-;;
-;; You should have received a copy of the GNU General Public License along with
-;; git-annex-configure. If not, see <https://www.gnu.org/licenses/>.
+;;; Copyright © 2023 aurtzy <aurtzy@gmail.com>
+;;;
+;;; This file is part of git-annex-configure.
+;;;
+;;; git-annex-configure is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by the
+;;; Free Software Foundation; either version 3 of the License, or (at your
+;;; option) any later version.
+;;;
+;;; git-annex-configure is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;;; more details.
+;;;
+;;; You should have received a copy of the GNU General Public License along
+;;; with git-annex-configure.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (git-annex-configure git annex config)
   #:use-module (git-annex-configure git annex repository)
